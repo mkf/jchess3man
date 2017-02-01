702499345d3b70f94865dc56cd43479ce05cf8de
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2017-02-01 22:27:02 +0100

    extends Throwable → extends Exception

--- pl.edu.platinum.archiet.jchess3man/src/pl/edu/platinum/archiet/jchess3man/engine/VectorAdditionFailedException.java
+++ pl.edu.platinum.archiet.jchess3man/src/pl/edu/platinum/archiet/jchess3man/engine/VectorAdditionFailedException.java
@@ -3,7 +3,7 @@ package pl.edu.platinum.archiet.jchess3man.engine;
 /**
  * Created by Michał Krzysztof Feiler on 28.01.17.
  */
-public class VectorAdditionFailedException extends Throwable {
+public class VectorAdditionFailedException extends Exception {
     public VectorAdditionFailedException(Pos from, Vector vector) {
     }
 }
