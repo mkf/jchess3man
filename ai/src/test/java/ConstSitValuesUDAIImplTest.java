5668e8f37ff38b386bbe6be97602ee4c6458a4d1
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2017-04-03 01:43:06 +0200

    annotated ConstSitVal test as disabled

--- ai/src/test/java/ConstSitValuesUDAIImplTest.java
+++ ai/src/test/java/ConstSitValuesUDAIImplTest.java
@@ -1,10 +1,12 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import pl.edu.platinum.archiet.jchess3man.engine.GameState;
 
 /**
  * Created by Michał Krzysztof Feiler on 18.03.17.
  */
+@Disabled
 class ConstSitValuesUDAIImplTest {
     ConstSitValuesUDAIImpl def = new ConstSitValuesUDAIImpl(
             null, null);
